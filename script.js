5a9d6cc7cdc34be2314438fe0515ec99c35e7bec
JayAldrin <j-p0371@students.uc-bcf.edu.ph>
2024-01-20 00:22:56 +0800

    about me, educ done

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleSkills() {
+    var skillsTemplate = document.getElementById('skills-template');
+    skillsTemplate.style.display = skillsTemplate.style.display === 'none' ? 'block' : 'none';
+}
